c08f97f23905f043ffd61fd9a7cbaf579abd8201
M. Mert Yildiran <me@mertyildiran.com>
2023-07-01 17:46:09 +0300

    :zap: Fix the logic of handling the `REACT_APP_HUB_PORT` environment variable

--- src/consts.tsx
+++ src/consts.tsx
@@ -1,8 +1,8 @@
 const HubHost = window.__RUNTIME_CONFIG__.REACT_APP_HUB_HOST.trim() ? window.__RUNTIME_CONFIG__.REACT_APP_HUB_HOST.trim() : window.location.hostname;
 const HubPort = window.__RUNTIME_CONFIG__.REACT_APP_HUB_PORT.trim() ? window.__RUNTIME_CONFIG__.REACT_APP_HUB_PORT.trim() : ":8898";
-const HubBaseUrl = `${window.location.protocol !== "https:" ? "http" : "https"}://${HubHost}${HubPort.length > 0 && HubPort[0] === "/" ? "" : ":"}${HubPort}`
-const HubWsUrl = `${window.location.protocol !== "https:" ? "ws" : "wss"}://${HubHost}${HubPort.length > 0 && HubPort[0] === "/" ? "" : ":"}${HubPort}/ws`
-const HubScriptLogsWsUrl = `${window.location.protocol !== "https:" ? "ws" : "wss"}://${HubHost}${HubPort.length > 0 && HubPort[0] === "/" ? "" : ":"}${HubPort}/scripts/logs`
+const HubBaseUrl = `${window.location.protocol !== "https:" ? "http" : "https"}://${HubHost}${HubPort.length > 0 && ["/", ":"].includes(HubPort[0]) ? "" : ":"}${HubPort}`
+const HubWsUrl = `${window.location.protocol !== "https:" ? "ws" : "wss"}://${HubHost}${HubPort.length > 0 && ["/", ":"].includes(HubPort[0]) ? "" : ":"}${HubPort}/ws`
+const HubScriptLogsWsUrl = `${window.location.protocol !== "https:" ? "ws" : "wss"}://${HubHost}${HubPort.length > 0 && ["/", ":"].includes(HubPort[0]) ? "" : ":"}${HubPort}/scripts/logs`
 
 const ColorGreen = "#d2fad2"
 const ColorRed = "#fad6dc"
